66fdb9abe085c7f7568442aa9b6e82a3f43cc477
aastharaut <aastharaut03@gmail.com>
2026-03-17 12:59:28 +0545

    Fix: Add Backend and Frontend as normal folders

---
+++ Backend/controllers/auth.ts
@@ -0,0 +1,6 @@
+import { Request, Response, NextFunction } from "express";
+const authController = {
+  signup: (req: Request, res: Response, next: NextFunction) => {},
+  login: (req: Request, res: Response, next: NextFunction) => {},
+};
+export default authController;
