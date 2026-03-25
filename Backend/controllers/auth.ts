8f2427888eabb8112cebd7e8e922db763cea9bf3
aastharaut <aastharaut03@gmail.com>
2026-03-25 16:47:18 +0545

    User model created and sequelize for database

--- Backend/controllers/auth.ts
+++ Backend/controllers/auth.ts
@@ -1,7 +1,14 @@
 import { Request, Response, NextFunction } from "express";
+import authService from "../services/authService";
 const authController = {
-  signup: (req: Request, res: Response, next: NextFunction) => {
-    res.send("from auth controller");
+  signup: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      let user = await authService.signup(req);
+      res.send(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({});
+    }
   },
   login: (req: Request, res: Response, next: NextFunction) => {},
 };
---
+++ Backend/models/User.ts
@@ -0,0 +1,54 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../connections/database";
+import { ROLES } from "../constants/roles";
+
+const User = sequelize.define(
+  "User",
+  {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: { min: 10, max: 100 },
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    role: {
+      type: DataTypes.ENUM(...Object.values(ROLES)),
+      defaultValue: ROLES.PATIENT,
+      allowNull: false,
+    },
+
+    phone: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+
+  {
+    tableName: "users",
+    underscored: true,
+    timestamps: true,
+  },
+);
+
+export default User;
